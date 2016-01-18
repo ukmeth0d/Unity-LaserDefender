802fdf2b3e81fb720f20ef119b1100db3c3f5710
Dean Smith <dean@Deans-MacBook-Pro.local>
2016-01-18 16:58:42 -0500

    LaserDefender - Lecture 105 complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float padding = 1;
+
+	private float xMin;
+	private float xMax;
+
+	// Use this for initialization
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance));
+
+		xMin = leftMost.x + padding;
+		xMax = rightMost.x - padding;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * (speed * Time.deltaTime);
+		} else if(Input.GetKey(KeyCode.RightArrow)){
+			transform.position += Vector3.right * (speed * Time.deltaTime);
+		}
+
+		// Restrict the player to the gamespace
+		float newX = Mathf.Clamp (transform.position.x, xMin, xMax);
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
+	}
+}
